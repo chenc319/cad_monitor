4631d6ece965f82bf4a3ed2cee44cf71b0455438
chenc319 <87655286+chenc319@users.noreply.github.com>
2026-02-13 20:46:14 -0600

    c

--- app_cad_repo.py
+++ app_cad_repo.py
@@ -8,13 +8,42 @@ from pathlib import Path
 import os
 DATA_DIR = os.getenv('DATA_DIR', 'data')
 
+### LOAD DATA ###
+with open(Path(DATA_DIR) / 'corra_df.pkl', 'rb') as file:
+    corra_df = pickle.load(file)
+with open(Path(DATA_DIR) / 'corra_compounded_df.pkl', 'rb') as file:
+    corra_compounded_df = pickle.load(file)
+with open(Path(DATA_DIR) / 'corra_5_df.pkl', 'rb') as file:
+    corra_5_df = pickle.load(file)
+with open(Path(DATA_DIR) / 'corra_25_df.pkl', 'rb') as file:
+    corra_25_df = pickle.load(file)
+with open(Path(DATA_DIR) / 'corra_75_df.pkl', 'rb') as file:
+    corra_75_df = pickle.load(file)
+with open(Path(DATA_DIR) / 'corra_95_df.pkl', 'rb') as file:
+    corra_95_df = pickle.load(file)
+with open(Path(DATA_DIR) / 'corra_total_volume_df.pkl', 'rb') as file:
+    corra_total_volume_df = pickle.load(file)
+with open(Path(DATA_DIR) / 'corra_trimmed_volume_df.pkl', 'rb') as file:
+    corra_trimmed_volume_df = pickle.load(file)
+
 ### ------------------------------------------------------------------------------------ ###
 ### ------------------------------------- CAD REPO ------------------------------------- ###
 ### ------------------------------------------------------------------------------------ ###
 
+corra_rate_complex = merge_dfs([corra_df,
+                                corra_5_df,
+                                corra_25_df,
+                                corra_75_df,
+                                corra_95_df]).dropna()
+corra_volume_df = merge_dfs([corra_total_volume_df,
+                             corra_trimmed_volume_df]).dropna()
 
-
-
+def plot_corra_rate_complex():
+    streamlit_plot(df=corra_rate_complex,
+                   columns_array=corra_rate_complex.columns,
+                   colors_array=["#0B2138", "#48DEE9",'#7EC0EE','#F9D15B','#F9C846'],
+                   graph_title='CORRA Rate Complex',
+                   y_axis_label='%')
 
 
 
--- cad_datapull.py
+++ cad_datapull.py
@@ -8,34 +8,56 @@ from pathlib import Path
 import os
 DATA_DIR = os.getenv('DATA_DIR', 'data')
 
+### ------------------------------------------------------------------------------------ ###
+### ------------------------------------- DATAPULL ------------------------------------- ###
+### ------------------------------------------------------------------------------------ ###
+
 ### CORRA RATE ###
 corra_df = get_boc_historical_timeseries(
     'AVG.INTWO',
-    '1990-01-01','corra')
+    'corra')
+with open(Path(DATA_DIR) / 'corra_df.pkl', 'wb') as file:
+    pickle.dump(corra_df, file)
+
 corra_compounded_df = get_boc_historical_timeseries(
     'CORRA.COMPOUNDED.INDEX',
-    '1990-01-01','corra')
+    'compounded_corra')
+with open(Path(DATA_DIR) / 'corra_compounded_df.pkl', 'wb') as file:
+    pickle.dump(corra_compounded_df, file)
+
 corra_5_df = get_boc_historical_timeseries(
     'CORRA_RATE_AT_PERCENTILE_5',
-    '1990-01-01','corra')
+    'corra_5')
+with open(Path(DATA_DIR) / 'corra_5_df.pkl', 'wb') as file:
+    pickle.dump(corra_5_df, file)
+
 corra_25_df = get_boc_historical_timeseries(
     'CORRA_RATE_AT_PERCENTILE_25',
-    '1990-01-01','corra')
+    'corra_25')
+with open(Path(DATA_DIR) / 'corra_25_df.pkl', 'wb') as file:
+    pickle.dump(corra_25_df, file)
+
 corra_75_df = get_boc_historical_timeseries(
     'CORRA_RATE_AT_PERCENTILE_75',
-    '1990-01-01','corra')
+    'corra_75')
+with open(Path(DATA_DIR) / 'corra_75_df.pkl', 'wb') as file:
+    pickle.dump(corra_75_df, file)
+
 corra_95_df = get_boc_historical_timeseries(
     'CORRA_RATE_AT_PERCENTILE_95',
-    '1990-01-01','corra')
+    'corra_95')
+with open(Path(DATA_DIR) / 'corra_95_df.pkl', 'wb') as file:
+    pickle.dump(corra_95_df, file)
 
 ### CORRA TRADING VOLUME ###
 corra_total_volume_df = get_boc_historical_timeseries(
     'CORRA_TOTAL_VOLUME',
-    'corra')
+    'corra_total_volume')
 with open(Path(DATA_DIR) / 'corra_total_volume_df.pkl', 'wb') as file:
     pickle.dump(corra_total_volume_df, file)
+
 corra_trimmed_volume_df = get_boc_historical_timeseries(
     'CORRA_TRIMMED_VOLUME',
-    'corra')
+    'corra_trimmed_volume')
 with open(Path(DATA_DIR) / 'corra_trimmed_volume_df.pkl', 'wb') as file:
     pickle.dump(corra_trimmed_volume_df, file)
